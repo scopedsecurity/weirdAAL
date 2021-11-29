e74526aff361edfe1696db90fde2bc29a626982f
scopedsecurity <zachhanley@gmail.com>
2021-11-29 13:26:27 -0500

    Update common.py

--- libs/utils/common.py
+++ libs/utils/common.py
@@ -5,7 +5,7 @@ def list_all_files(directory):
 
   array = []
 
-  path ="modules"
+  path = directory
   for (dirpath, dirnames, filenames) in os.walk(path):
       if ( not (dirpath == os.path.basename(directory)) and
       (os.path.isdir(dirpath))
@@ -13,5 +13,8 @@ def list_all_files(directory):
         for file in filenames:
             list_path_name = dirpath.split('/')
             if not (file.startswith('.')):
-                array.append(".".join(list_path_name) + "." + os.path.splitext(file)[0])
+                module = ".".join(list_path_name) + "." + os.path.splitext(file)[0]
+                module = module.lstrip(".opt.weirdAAL.")
+                array.append(module)
+                
   return array
